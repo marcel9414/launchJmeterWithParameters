74036ccbd2c15fc8ab45edcc783464868e9724ed
marcel9414 <marcelgutierrez1994@gmail.com>
2018-03-05 19:13:50 +0100

    new

---
+++ jmeterApplication/src/main/java/tfg/jmeterApplication/App.java
@@ -0,0 +1,14 @@
+package tfg.jmeterApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Pruena Jmeter" );
+    }
+}
---
+++ launchJmeterAplication/src/main/java/tfg/launchJmeterAplication/App.java
@@ -0,0 +1,13 @@
+package tfg.launchJmeterAplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
